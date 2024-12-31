441d8db2946971b0efc7cbc3f6a2472653f1d329
tsaninur <tsanisunny22@gmail.com>
2024-12-31 16:25:57 +0700

    fix blank page

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/app-react">  {/* Set basename for GitHub Pages */}
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
